b14b9ec7e19ed65fb44c1173a9d26a237ab83445
Vicki-Alden <vic.alden23@gmail.com>
2024-01-25 16:52:25 -0800

    create list view

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlashcardArray } from 'react-quizlet-flashcard';
 import { Container } from '@mui/material';
+import ListIcon from '@mui/icons-material/List';
+import CardIcon from '@mui/icons-material/CropSquare';
 import { cards } from './cards';
 
 function App() {
+  const [isCardView, setIsCardView] = useState(true);
   return (
-    <Container
-      style={{
-        textAlign: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: '20px',
-      }}
-    >
-      <FlashcardArray cards={cards} />
+    <Container>
+      <Container
+        style={{
+          textAlign: 'center',
+          marginTop: '20px',
+        }}
+      >
+        <ListIcon
+          style={{ fontSize: '30px', marginRight: '5px', cursor: 'pointer' }}
+          onClick={() => setIsCardView(false)}
+        />
+        <CardIcon
+          style={{ fontSize: '30px', marginLeft: '5px', cursor: 'pointer' }}
+          onClick={() => setIsCardView(true)}
+        />
+      </Container>
+
+      {isCardView ? (
+        <Container
+          style={{
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '20px',
+          }}
+        >
+          <FlashcardArray cards={cards} />
+        </Container>
+      ) : (
+        <ul
+          style={{
+            marginTop: '40px',
+          }}
+        >
+          {cards.map((c) => (
+            <li
+              style={{
+                marginBottom: '20px',
+              }}
+            >
+              <p>{c.q}</p>
+              <p>{c.a}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </Container>
   );
 }
